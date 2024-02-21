c4e69a512d82b3c94a1b9a906b362b3490438f78
Matos52 <Matej.Regec@gmail.com>
2024-02-21 11:03:37 +0100

    Implenting new features to the game, finnishing the code and final refactoring.

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -4,24 +4,19 @@ const initialBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard({ onSelectSquare, turns }) {
-  let gameBoard = initialBoard;
-
-for (const turn of turns) {
-  const { square, player } = turn;
-  const { row, col } = square;
-
-  gameBoard[row][col] = player;
-}
+export default function GameBoard({ onSelectSquare, board }) {
 
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol !== null}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
                   {playerSymbol}
                 </button>
               </li>
